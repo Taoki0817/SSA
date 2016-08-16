9201ec9f6356f9ce95c23c9604b8bf7ddd353565
iwatani <iwatani@fortiservices.com>
2016-08-16 16:58:06 +0900

    '岩谷プッシュ'

--- src/main/java/ssa/Controller.java
+++ src/main/java/ssa/Controller.java
@@ -2,13 +2,21 @@ package ssa;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 
 public class Controller {
+	
 	@RequestMapping("")
 	public String index(Model model){
-		model.addAttribute("name", "Aoki");
-		return "index";
+		return "prototype";
+	}
+		
+	@RequestMapping(value="/post" ,method = RequestMethod.POST)
+	public String serch(Model model, @RequestParam("name") String name){
+		model.addAttribute("name", name);
+		return "prototype";
 	}
 }
